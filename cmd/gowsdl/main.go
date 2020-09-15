075abf5a2e774508c76576667ae98e1972e60f3f
SemyonL <semyonl1995@gmail.com>
2020-09-15 18:33:29 +0300

    changed mod

--- cmd/gowsdl/main.go
+++ cmd/gowsdl/main.go
@@ -54,7 +54,7 @@ import (
 	"log"
 	"os"
 
-	gen "github.com/hooklift/gowsdl"
+	gen "github.com/SemyonL95/gowsdl"
 )
 
 // Version is initialized in compilation time by go build.
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/hooklift/gowsdl
+module github.com/SemyonL95/gowsdl
 
 go 1.15
+
+require github.com/hooklift/gowsdl v0.4.0
